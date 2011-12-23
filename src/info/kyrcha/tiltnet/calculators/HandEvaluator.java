dff86466d731d2fff167397a916b3370bdfd3797
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2011-12-23 18:19:26 +0200

    Add couple of classes

---
+++ src/info/kyrcha/tiltnet/calculators/HandEvaluator.java
@@ -0,0 +1,7 @@
+public class HandEvaluator {
+    
+    public final static int rank(Hand hand) {
+	// should return a unique rank for each hand
+    }
+
+}
--- src/info/kyrcha/tiltnet/calculators/HandStrength.java
+++ src/info/kyrcha/tiltnet/calculators/HandStrength.java
@@ -1,7 +1,17 @@
 public class HandStrength {
 
-    public double handStrength(String hand) {
-	return 0;
+    public double handStrength(String holeCards, String boardCards) {
+	double ahead = 0;
+	double tied = 0;
+	double behind = 0;
+	// Rank my hand based on hole and board cards
+	int rank = 0;
+	// for each case(oppCards)
+	int oppRank = 0;
+	if(rank > oppRank) ahead++;
+	else if(rank == oppRank) tied++;
+	else behind++;
+	return (ahead + tied / 2) / (ahead + tied + behind);
     }
 
 }
